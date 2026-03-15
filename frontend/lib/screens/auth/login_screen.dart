69f5bba8ffa07928f35446c5011470b04234cf01
Geethma Piyaratne <geethmapiyaratne285@gmail.com>
2026-03-15 11:50:00 +0530

    Replace signup_page import with register_screen

--- frontend/lib/screens/auth/login_screen.dart
+++ frontend/lib/screens/auth/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets.dart';
-import 'signup_page.dart';
+import 'register_screen.dart';
 import 'auth_widgets.dart'; 
 
 class LoginPage extends StatelessWidget {
